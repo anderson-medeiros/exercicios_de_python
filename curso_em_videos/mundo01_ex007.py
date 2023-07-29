3883402798befbcb037cf35523a6476ac86cca81
Anderson Medeiros <dev.andersonmedeiros@gmail.com>
2023-07-29 17:13:10 -0400

    Resolução do 7º exercício

---
+++ curso_em_videos/mundo01_ex007.py
@@ -0,0 +1,12 @@
+"""
+# Média Aritimética
+
+Desenvolva um programa que leia as duas notas de um aluno. Calcule e mostre a
+sua média.
+"""
+
+nota1 = float(input('Primeira nota do aluno: '))
+nota2 = float(input('Segunda nota do aluno: '))
+media = (nota1 + nota2) / 2
+print(f'A média entre {nota1} e {nota2} é igual a {media}')
+
