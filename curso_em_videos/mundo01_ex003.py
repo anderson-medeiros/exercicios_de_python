813fbd2c15a214d8135d9893a07ff0642da3fb79
Anderson Medeiros <dev.andersonmedeiros@gmail.com>
2023-07-29 17:11:08 -0400

    Resolução do 3º exercício

---
+++ curso_em_videos/mundo01_ex003.py
@@ -0,0 +1,10 @@
+"""
+# Somando dois números
+
+Crie um programa que leia dois números e mostre a soma entre eles.
+"""
+
+n1 = int(input('Insira o primeiro número: '))
+n2 = int(input('Insira o segundo número: '))
+print(f'A soma entre {n1} e {n2} é {n1 + n2}')
+
