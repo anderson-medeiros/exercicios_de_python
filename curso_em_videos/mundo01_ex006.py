9ae4e2e1c0bba8c1c53d353799f121f575b76855
Anderson Medeiros <dev.andersonmedeiros@gmail.com>
2023-07-29 17:12:47 -0400

    Resolução do 6º exercício

---
+++ curso_em_videos/mundo01_ex006.py
@@ -0,0 +1,13 @@
+"""
+# Dobro, Triplo, Raiz Quadrada
+
+Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz
+quadrada.
+"""
+
+n = int(input('Digite um número: '))
+print(f'''\
+O dobro de {n} vale {n * 2}
+O triplo de {n} vale {n * 3}
+A raiz quadrada de {n} é igual a {n ** 0.5}''')
+
