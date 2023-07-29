44f458ba9acd19b641c35ee9cb6cda8c04b4226d
Anderson Medeiros <dev.andersonmedeiros@gmail.com>
2023-07-29 17:12:13 -0400

    Resolução do 5º exercício

---
+++ curso_em_videos/mundo01_ex005.py
@@ -0,0 +1,11 @@
+"""
+# Antecessor e Sucessor
+
+Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e
+o seu antecessor.
+"""
+
+n = int(input('Digite um número: '))
+print(
+    f'analisando o valor {n}, seu antecessor é {n - 1} e o sucessor é {n + 1}')
+
