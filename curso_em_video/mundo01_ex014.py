bab102e0da936b1b459030bc7b99bff6cd42e6a5
Anderson Medeiros <dev.andersonmedeiros@gmail.com>
2023-08-05 20:42:59 -0400

    Resolução do 14º exercício

---
+++ curso_em_video/mundo01_ex014.py
@@ -0,0 +1,12 @@
+"""
+# Conversor de Temperaturas
+
+Escreve um programa que converta uma temperatura digitada em °C e converta
+para °F·
+"""
+
+celsius = float(input('Informe a temperatura em °C: '))
+fahrenheit = celsius * 1.8 + 32
+
+print(f'A temperatura de {celsius}°C corresponde a {fahrenheit}°F')
+
