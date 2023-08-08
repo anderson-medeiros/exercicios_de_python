a718be80cb0d1052de6abddeed6586d367873eb4
Anderson Medeiros <dev.andersonmedeiros@gmail.com>
2023-08-07 21:35:01 -0400

    Resolução do 20º exercício

---
+++ curso_em_video/mundo01_ex020.py
@@ -0,0 +1,17 @@
+"""
+# Sorteando uma ordem na lista
+
+O mesmo professor do desafio anterior quer sortear a ordem de apresentação de
+trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos e
+mostre a ordem sorteada.
+"""
+from random import sample
+
+aluno1 = input('Primeiro aluno: ')
+aluno2 = input('Segundo aluno: ')
+aluno3 = input('Terceiro aluno: ')
+aluno4 = input('Quarto aluno: ')
+alunos = [aluno1, aluno2, aluno3, aluno4]
+
+print(sample(alunos, k=len(alunos)))
+
