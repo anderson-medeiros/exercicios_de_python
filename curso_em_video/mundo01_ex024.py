20be605100f031bb748a9ed657e4ce72f6f812e9
Anderson Medeiros <dev.andersonmedeiros@gmail.com>
2023-08-09 21:52:17 -0400

    Resolução do 24º exercício

---
+++ curso_em_video/mundo01_ex024.py
@@ -0,0 +1,11 @@
+"""
+# Verificando as primeiras letras de um texto
+
+Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com 
+o nome "SANTO".
+"""
+
+
+cidade = input('Em que cidade você nasceu? ').strip()
+print(cidade[:5].upper() == 'SANTO')
+
