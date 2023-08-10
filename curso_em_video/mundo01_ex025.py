64beb497c38f06d5a31d72bc2b34ba5ba2038d2b
Anderson Medeiros <dev.andersonmedeiros@gmail.com>
2023-08-09 21:54:31 -0400

    Resolução do 25º exercício

---
+++ curso_em_video/mundo01_ex025.py
@@ -0,0 +1,11 @@
+"""
+# Procurando uma string dentro de outra
+
+Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no 
+nome.
+"""
+
+
+nome = input('Qual é o seu nome completo? ').strip().upper()
+print('SILVA' in nome)
+
